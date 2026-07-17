788d442bb70d7469a567f20ee603d0a4705098cf
Hector Salinas <hmsalinas31@outlook.com>
2026-07-17 11:23:04 -0500

    feat: implement metaballs generator

--- src/cavegenlabs/algorithms/metaballs/generator.py
+++ src/cavegenlabs/algorithms/metaballs/generator.py
@@ -1,10 +1,17 @@
+from dataclasses import dataclass
+from math import hypot
 from random import Random
 
 from cavegenlabs.algorithms.metaballs.config import MetaballsConfig
 from cavegenlabs.domain.models.cave_map import CaveMap
 from cavegenlabs.domain.models.tile import Tile
 
-
+@dataclass(frozen=True)
+class Metaball:
+    center_x: int
+    center_y: int
+    radius: int
+    
 class MetaballsGenerator:
     def generate(
         self,
@@ -16,10 +23,75 @@ class MetaballsGenerator:
             for _ in range(config.height)
         ]
 
-        # IMPLEMENT!!!
+        metaballs = self._generate_metaballs(
+            config=config,
+            rng=rng,
+        )
+
+        self._carve_metaballs(
+            grid=grid,
+            config=config,
+            metaballs=metaballs,
+        )
 
         return CaveMap(
             width=config.width,
             height=config.height,
             cells=tuple(tuple(row) for row in grid),
         )
+
+    def _generate_metaballs(
+        self,
+        config: MetaballsConfig,
+        rng: Random
+    ) -> list[Metaball]:
+        metaballs: list[Metaball] = []
+
+        for _ in range(config.metaball_count):
+            metaballs.append(
+                Metaball(
+                    center_x=rng.randrange(config.width),
+                    center_y=rng.randrange(config.height),
+                    radius=rng.randint(
+                        config.min_radius,
+                        config.max_radius,
+                    )
+                )
+            )
+
+        return metaballs
+
+    def _carve_metaballs(
+        self,
+        grid: list[list[Tile]],
+        config: MetaballsConfig,
+        metaballs: list[Metaball]
+    ) -> None:
+        for y in range(config.height):
+            for x in range(config.width):
+                influence = 0
+                for metaball in metaballs:
+                    influence += self._calculate_influence(
+                        metaball=metaball,
+                        x=x,
+                        y=y 
+                    )
+
+                if influence >= config.threshold:
+                    grid[y][x] = Tile.AIR
+
+    def _calculate_influence(
+        self,
+        metaball: Metaball,
+        x: int,
+        y: int,
+    ) -> float:
+        distance = hypot(
+            x - metaball.center_x,
+            y - metaball.center_y
+        )
+
+        if distance == 0:
+            return float("inf")
+        
+        return (metaball.radius ** 2) / (distance ** 2)
