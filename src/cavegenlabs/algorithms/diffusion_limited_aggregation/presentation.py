5e81f1a1726f7cb8f9d6e398a90523b15a629489
Hector Salinas <hmsalinas31@outlook.com>
2026-07-17 15:05:48 -0500

    feat: create diffusion limited aggregation presenation.py

---
+++ src/cavegenlabs/algorithms/diffusion_limited_aggregation/presentation.py
@@ -0,0 +1,13 @@
+from cavegenlabs.presentation.inputs.integer_parameter_input import IntegerParameterInput
+from cavegenlabs.presentation.inputs.parameter_input import ParameterInput
+
+
+def create_inputs() -> tuple[ParameterInput, ...]:
+    return (
+        IntegerParameterInput(
+            key="particle_count",
+            label="Particle Count",
+            default=10,
+            minimum=1,
+        )
+    )
